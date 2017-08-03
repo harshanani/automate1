d07d722f4fecc0e715de02f28659a4cd982f0761
harshanani <vardhanharsha@gmail.com>
2017-08-03 06:01:51 +0000

    nexus integration

--- src/main/java/com/automate1/App.java
+++ src/main/java/com/automate1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!iiiiii" );
+        System.out.println( "Hello World! Nexus integration" );
     }
 }
